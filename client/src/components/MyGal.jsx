89f96bdf3542fdea53b7250d380db7b7d4789d67
varnaraj <e17358@eng.pdn.ac.lk>
2022-12-31 09:27:51 +0530

    update

--- client/src/components/MyGal.jsx
+++ client/src/components/MyGal.jsx
@@ -91,7 +91,7 @@ function MyGal() {
     <div>
       <Box sx={{ display: "flex", justifyContent: "space-evenly" }}>
         <Typography fontWeight={"bold"} fontSize={40}>
-          My Gallary
+          Mathu Model Gallary
         </Typography>
       </Box>
       <Box mt={4} sx={{ display: "flex", justifyContent: "space-evenly" }}>
@@ -168,7 +168,10 @@ function MyGal() {
 
                 <img
                   width={425}
-                  src={"http://localhost:8080/files/" + item.name}
+                  src={
+                    "http://ec2-13-213-36-94.ap-southeast-1.compute.amazonaws.com:8080/files/" +
+                    item.name
+                  }
                   // src={item.imgSrc}
                   alt={item.id}
                 />
--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://ec2-13-213-36-94.ap-southeast-1.compute.amazonaws.com:8080",
   headers: {
     "Content-type": "application/json",
   },
