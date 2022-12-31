e8e6d4c325c3744e84a0da42d4014d2a5fefec91
varnaraj <64960978+varnaraj@users.noreply.github.com>
2022-12-31 09:59:18 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
-import Gallary from "./components/Gallary";
+
 import MyGal from "./components/MyGal";
 
-// import ImageUploadCard from "./components/ImageUploadCard";
-import UploadImages from "./components/UploadImages";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // import SignUp from './components/SignUp'
